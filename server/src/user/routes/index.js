35c9fa4d4f446e36ac60fa2c98d821f6c17a3f54
Ajoy10 <achu.ajoy10@gmail.com>
2023-02-26 16:10:33 +0530

    Implemented logout

--- server/src/user/routes/index.js
+++ server/src/user/routes/index.js
@@ -149,6 +149,36 @@ router.post("/refresh-token", (req, res, next) => {
   }
 });
 
+router.get("/logout", verifyUser, (req, res) => {
+  const { signedCookies = {} } = req;
+
+  const { refreshToken } = signedCookies;
+
+  User.findById(req.user._id).then(
+    (user) => {
+      const tokenIndex = user.refreshToken.findIndex(
+        (item) => item.refreshToken === refreshToken
+      );
+
+      if (tokenIndex !== -1) {
+        user.refreshToken.id(user.refreshToken[tokenIndex]._id).remove();
+      }
+
+      user.save((err, user) => {
+        if (err) {
+          res.statusCode = 500;
+          res.send(err);
+        } else {
+          res.clearCookie("refreshToken", COOKIE_OPTIONS);
+          res.send({ success: true });
+        }
+      });
+    },
+
+    (err) => next(err)
+  );
+});
+
 // Change later, now for testing verify
 router.get(
   "/me",
