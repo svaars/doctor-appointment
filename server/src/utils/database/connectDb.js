15187a72bc52a082ab65bcee6cf747c29e9bb199
Ajoy10 <achu.ajoy10@gmail.com>
2023-04-25 17:04:15 +0530

    Added db name in env

--- server/src/utils/database/connectDb.js
+++ server/src/utils/database/connectDb.js
@@ -6,6 +6,7 @@ mongoose
   .connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    dbName: process.env.DB_NAME,
   })
   .then(() => {
     console.log("Database connected!");
